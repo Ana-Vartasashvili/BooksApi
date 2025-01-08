532cfe69e6f6f38f4eb415036a0046a92d030692
ana.vartasashvili <ana.vartasashvili@europebet.com>
2025-01-08 17:34:47 +0400

    add test for getAllBooks route

--- BooksApi/obj/Debug/net8.0/BooksApi.AssemblyInfo.cs
+++ BooksApi/obj/Debug/net8.0/BooksApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a66dafc6cebbdd9612afc43e469b6e1c431a4ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce961a445ac55d3aa162dab7c20764bd2b2483d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
