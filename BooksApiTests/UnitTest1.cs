fe3f65d61c44da346d0a57bb4948393b75525bde
ana.vartasashvili <ana.vartasashvili@europebet.com>
2025-01-08 20:24:48 +0400

    add test for get all books endpoint

---
+++ BooksApiTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using System.Net.Http.Json;
+using BooksApi;
+using BooksApi.Books;
+
+namespace BooksApiTests;
+
+public class BasicTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly CustomWebApplicationFactory _factory;
+
+    public BasicTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+    }
+    
+    [Fact]
+    public async Task GetAllBooks_ReturnsOkResult()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/books");
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Failed to get books: {content}");
+        }
+        var books = await response.Content.ReadFromJsonAsync<IEnumerable<GetBookDto>>();
+        
+        Assert.NotEmpty(books);
+    }
+}
