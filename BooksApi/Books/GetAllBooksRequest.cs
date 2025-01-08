310f41c9b1da49ed65841ca2e3e10444cef99280
ana.vartasashvili <ana.vartasashvili@europebet.com>
2025-01-08 22:01:51 +0400

    add fluent validation

--- BooksApi/Books/GetAllBooksRequest.cs
+++ BooksApi/Books/GetAllBooksRequest.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 namespace BooksApi.Books;
 
 public class GetAllBooksRequest
@@ -5,4 +7,13 @@ public class GetAllBooksRequest
     public int? Page { get; set; }
     public int? PageSize { get; set; }
     public string? Title { get; set; }
+}
+
+public class GetAllBooksRequestValidator : AbstractValidator<GetAllBooksRequest>
+{
+    public GetAllBooksRequestValidator()
+    {
+        RuleFor(x => x.Page).GreaterThanOrEqualTo(1).WithMessage("Page must be greater than or equal to 1.");
+        RuleFor(x=>x.PageSize).GreaterThanOrEqualTo(1).LessThanOrEqualTo(100).WithMessage("You can not get more than 100 items.");
+    }
 }
--- BooksApi/Program.cs
+++ BooksApi/Program.cs
@@ -1,4 +1,5 @@
 using BooksApi;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,12 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "BooksApi.xml"));
 });
-builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<FluentValidationFilter>();
+});
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
