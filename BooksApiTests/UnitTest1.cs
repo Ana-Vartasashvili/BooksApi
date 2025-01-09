1ab937efbd16b60a2287318bb4fbbf732a6e0349
ana.vartasashvili <ana.vartasashvili@europebet.com>
2025-01-09 20:26:54 +0400

    add test for create book endpoint

--- BooksApiTests/UnitTest1.cs
+++ BooksApiTests/UnitTest1.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Json;
 using BooksApi;
 using BooksApi.Books;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksApiTests;
 
@@ -58,5 +59,65 @@ public class BasicTests : IClassFixture<CustomWebApplicationFactory>
         var response = await client.GetAsync("/books/999999");
         
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateBook_ReturnsCreatedResult()
+    {
+        var client = _factory.CreateClient();
+        var book = new CreateBookDto
+        {
+            Title = "Book test",
+            Author = "Author test",
+            ISBN = "123456789",
+
+        };
+        
+        var response = await client.PostAsJsonAsync("/books", book);
+        
+        response.EnsureSuccessStatusCode();
+        
+        var createdBook = await response.Content.ReadFromJsonAsync<GetBookDto>();
+        Assert.NotNull(createdBook);
+    }
+
+    [Fact]
+    public async Task CreateBook_ReturnsBadRequestResult()
+    {
+        var client = _factory.CreateClient();
+        var invalidBook = new CreateBookDto();
+        
+        var response = await client.PostAsJsonAsync("/books", invalidBook);
+        var problemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.NotNull(problemDetails);
+        Assert.Contains("Title", problemDetails.Errors.Keys);
+        Assert.Contains("Author", problemDetails.Errors.Keys);
+        Assert.Contains("ISBN", problemDetails.Errors.Keys);
+        Assert.Contains("'Title' must not be empty.", problemDetails.Errors["Title"]);
+        Assert.Contains("'Author' must not be empty.", problemDetails.Errors["Author"]);
+        Assert.Contains("'ISBN' must not be empty.", problemDetails.Errors["ISBN"]);
+    }
+
+    [Fact]
+    public async Task CreateBook_ReturnsBadRequestWhenBookAlreadyExists()
+    {
+        var client = _factory.CreateClient();
+        var existingBookTitle = "Book 1";
+        var book = new CreateBookDto
+        {
+            Title = existingBookTitle,
+            Author = "Author test",
+            ISBN = "111111"
+        };
+        
+        var response = await client.PostAsJsonAsync("/books", book);
+        var problemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.NotNull(problemDetails);
+        Assert.Contains("Title", problemDetails.Errors.Keys);
+        Assert.Contains("Book with this title already exists", problemDetails.Errors["Title"]);
     }
 }
